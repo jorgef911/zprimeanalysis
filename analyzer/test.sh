501e20032aa7085089040d733af1dcdbaf19dc53
Jorge Fraga <jf.fraga@uniandes.edu.co>
2019-11-19 05:59:44 -0600

    initial

---
+++ analyzer/test.sh
@@ -0,0 +1,12 @@
+#root://cmsxrootd.fnal.gov///store/mc/RunIISummer16NanoAODv4/DYJetsToLL_M-50_HT-100to200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/NANOAODSIM/PUMoriond17_Nano14Dec2018_102X_mcRun2_asymptotic_v6-v1/40000/185A0751-C4F2-BC4B-8FAD-F6014D87938A.root -out DYJetsToLL_M-50_HT-100to200_TuneCUETP8M1_13TeVMLM_TeV_MG_MC_0.root
+
+#root://cmsxrootd.fnal.gov///store/mc/RunIISummer16NanoAODv4/DYJetsToLL_M-50_HT-100to200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/NANOAODSIM/PUMoriond17_Nano14Dec2018_102X_mcRun2_asymptotic_v6-v1/40000/84802AA2-D430-ED47-9B13-330AA07299D2.root
+
+#root://cmsxrootd.fnal.gov///store/mc/RunIISummer16NanoAODv4/DYJetsToLL_M-50_HT-100to200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/NANOAODSIM/PUMoriond17_Nano14Dec2018_102X_mcRun2_asymptotic_v6-v1/40000/CAD9D768-5732-8448-BDBB-9326F9B2A67C.root
+
+
+
+
+./Analyzer -in root://cmsxrootd.fnal.gov///store/data/Run2016B_ver2/SingleMuon/NANOAOD/Nano14Dec2018_ver2-v1/280000/AF74CDFF-09E7-2446-94D8-E7BFAB5893BD.root -out outputtest.root -C PartDet -t
+
+#./Analyzer -in root://cmsxrootd.fnal.gov///store/mc/RunIISummer16NanoAODv4/DYJetsToLL_M-50_HT-100to200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/NANOAODSIM/PUMoriond17_Nano14Dec2018_102X_mcRun2_asymptotic_v6-v1/40000/185A0751-C4F2-BC4B-8FAD-F6014D87938A.root -out outputtest.root -C PartDet -t
